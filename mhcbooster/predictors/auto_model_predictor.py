c5a5178b8e36d6b6fbfa54754eeaa811a226023d
Ruimin Wang <ruimin.wang@yale.edu>
2025-05-30 11:40:40 -0400

    [Optimize] SVD in autopred

--- mhcbooster/predictors/auto_model_predictor.py
+++ mhcbooster/predictors/auto_model_predictor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 
 
@@ -19,11 +20,11 @@ def _pred_best_combination(score_matrix, predictor_type=None, max_predictors=4):
     n_components = min(max(2, n_components), max_predictors)
 
     ### Second round decomposition: choose the most important scores
-    svd = TruncatedSVD(n_components=n_components)
-    svd.fit(score_matrix)
+    # svd = TruncatedSVD(n_components=n_components)
+    # svd.fit(score_matrix)
 
     # Compute the L2 norm of each column (Euclidean norm) for "importance"
-    importance_matrix = np.dot(np.diag(svd.singular_values_), svd.components_)
+    importance_matrix = np.dot(np.diag(svd.singular_values_[:n_components]), svd.components_[:n_components,])
     importance = np.linalg.norm(importance_matrix, axis=0)
     sort_indices = np.argsort(-importance)
     best_predictors = list(predictors[sort_indices][: n_components])
@@ -39,3 +40,7 @@ def predict_best_combination(feature_matrix):
     ms2_predictors = _pred_best_combination(ms2_scores, predictor_type='MS2')
 
     return rt_predictors, ms2_predictors
+
+if __name__ == '__main__':
+    feature_matrix = pd.read_csv('/mnt/d/workspace/mhc-booster/experiment/JY_1_10_25M/best/JY_Class1_25M_DDA_60min_Slot1-12_1_552_MHCBooster/all_features.tsv', sep='\t')
+    predict_best_combination(feature_matrix)
